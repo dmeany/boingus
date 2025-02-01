90ebcb67efe1aa14625ee89663f39cd44e7fe5d2
Doug Meany <dmeany@mines.edu>
2025-01-31 22:06:19 -0700

    bleh

--- lab0.ml
+++
@@ -1,9 +0,0 @@
-(* Define a function for the main entry point *)
-let main () =
-  (* TODO: change to print "Hello, world!" instead of "Goodbye"*)
-  print_string "Hello, world!\n"
-;;
-
-
-(* Call main *)
-main ()
